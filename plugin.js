993a3d5eec9ff77ae30401a577be04587705d0c1
nka11 <nka11@nka.me>
2011-02-26 11:37:36 +0100

    jzaefferer's treeview imported from http://github.com/jzaefferer/jquery-treeview

--- plugin.js
+++ plugin.js
@@ -9,6 +9,12 @@ jQuery.extend(true,DebuggerPlugin,{
 		 * Initialize plugin
 		 */
 		init: function() {
+			jQuery('head').append('<link rel="stylesheet" href="' 
+					+ GENTICS.Aloha.settings.base 
+					+ 'plugins/me.nka.aloha.Debugger/css/jquery.treeview.css" />');
+			jQuery('head').append('<script type="text/javascript" src="' 
+					+ GENTICS.Aloha.settings.base 
+					+ 'plugins/me.nka.aloha.Debugger/lib/jquery.treeview.js"></script>');
 			var that = this;
 			jQuery('body').append('<div id="NKA_debugger"><div id="NKA_debug_editable"></div><div id="NKA_debug_tabs"></div><div id="NKA_debug_console"></div></div>');
 			jQuery('#NKA_debug_tabs').append('<ul><li><a href="#NKA_range_tree_console">SelectionRange</a></li><li><a href="#NKA_debug_console">Log Console</a></li></ul>');
@@ -22,7 +28,7 @@ jQuery.extend(true,DebuggerPlugin,{
 			jQuery('#NKA_debugger').dialog({'title': 'Aloha debugger window'});
 			jQuery('#NKA_debug_tabs').tabs();
 			GENTICS.Aloha.EventRegistry.subscribe(GENTICS.Aloha, 'selectionChanged', function(event, rangeObject) {
-				jQuery('#NKA_range_tree_console').empty().append(that.renderRangeTree(rangeObject.getRangeTree()));
+				jQuery('#NKA_range_tree_console').empty().append(that.renderRangeTree(rangeObject.getRangeTree()).treeview({collapsed: true}));
 				jQuery('#NKA_debug_editable').empty().append('<nav/>').append(GENTICS.Aloha.activeEditable.getId());
 //				var logHistory = GENTICS.Aloha.Log.getLogHistory();
 //				var len = logHistory.length;
@@ -36,28 +42,28 @@ jQuery.extend(true,DebuggerPlugin,{
 		renderRangeTree: function(tree) {
 			var that = this;
 			if (tree && tree.length != 0) {
-				var list = jQuery('<div style="padding-left: 1em;"/>');
+				var list = jQuery('<ul/>');
 				jQuery.each(tree, function(index, item) {
 					if (item.type == 'none') {
 						return true;
 					}
 					if (item.domobj) {
-						var li = jQuery('<a href="#"/>');
-						list.append(jQuery('<h3/>').append(li));
+						var li = jQuery('<li/>');
+						list.append(li);
 						var text = item.domobj.nodeName + ' (' + item.type + ') ';
-						li.text(text);
+						li.append(jQuery('<span/>').text(text));
 						if (item.domobj.nodeType == 3) {
-							var i = jQuery('<p style="margin-left: 1em;"/>');
+							var i = jQuery('<li/>');
 							if (item.type == 'partial') {
 								i.text(item.domobj.nodeValue.substring(item.startOffset, item.endOffset));
 							} else {
 								i.text(item.domobj.nodeValue);
 							}
-							list.append(jQuery('<div/>').append(i));
+							li.append(jQuery('<ul/>').append(i));
 						}
 						var sub = that.renderRangeTree(item.children);
 						if (sub) {
-							list.append(sub);
+							li.append(sub);
 						}
 					} else {
 						return true;
